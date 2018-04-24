41c2c7b175b14e5cb51ea2179ee2480d1f38cc2e
yvan <57882634@qq.com>
2018-04-24 19:06:51 +0800

    demo01

--- demo01/gulpfile.js
+++ demo01/gulpfile.js
@@ -1,17 +1,26 @@
+var gulp = require('gulp');
 var combiner = require('stream-combiner2');
 var uglify = require('gulp-uglify');
-var gulp = require('gulp');
+var rename = require('gulp-rename');
+var del = require('del');
 
-gulp.task('test', function() {
+gulp.task('combine', ['clean'], function() {
   var combined = combiner.obj([
-    gulp.src('bootstrap/js/*.js'),
-    uglify(),
-    gulp.dest('public/bootstrap')
+    gulp.src('src/**/*.js'),
+    uglify(), rename({suffix: '.min'}),
+    gulp.dest('dist/js/')
   ]);
 
-  // 任何在上面的 stream 中发生的错误，都不会抛出，
-  // 而是会被监听器捕获
+  // 任何在上面的 stream 中发生的错误，都不会抛出，而是会被监听器捕获
   combined.on('error', console.error.bind(console));
 
   return combined;
 });
+
+gulp.task('clean', function(cb) {
+  return del([
+    'dist/'
+  ], cb)
+});
+
+gulp.task('default', ['combine']);
