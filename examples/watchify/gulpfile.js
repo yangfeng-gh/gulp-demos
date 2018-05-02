d2327de8c7d7f6d941ae7796855fee6515f06a33
yangfeng_osc <57882634@qq.com>
2018-05-02 13:08:56 +0800

    incremental

--- examples/watchify/gulpfile.js
+++ examples/watchify/gulpfile.js
@@ -1,16 +1,41 @@
+'use strict';
+
+var watchify = require('watchify');
+var browserify = require('browserify');
 var gulp = require('gulp');
-var del = require('del');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var assign = require('lodash.assign');
 
-gulp.task('compile', function(cb) {
-    gulp.src('src/**/*')
-    .pipe(gulp.dest('dist/'))
-})
+// 在这里添加自定义 browserify 选项
+var customOpts = {
+  entries: ['./src/index.js'],
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
 
-gulp.task('clean', function(cb) {
-    del([
-        'dist/',
-        // '!dist/deploy.json'
-    ], cb);
-})
+// 在这里加入变换操作
+// 比如： b.transform(coffeeify);
 
-gulp.task('default', ['clean', 'compile'])
+gulp.task('js', bundle); // 这样你就可以运行 `gulp js` 来编译文件了
+b.on('update', bundle); // 当任何依赖发生改变的时候，运行打包工具
+b.on('log', gutil.log); // 输出编译日志到终端
+
+function bundle() {
+  return b.bundle()
+    // 如果有错误发生，记录这些错误
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('bundle.js'))
+    // 可选项，如果你不需要缓存文件内容，就删除
+    .pipe(buffer())
+    // 可选项，如果你不需要 sourcemaps，就删除
+    .pipe(sourcemaps.init({loadMaps: true})) // 从 browserify 文件载入 map
+       // 在这里将变换操作加入管道
+    .pipe(sourcemaps.write('./')) // 写入 .map 文件
+    .pipe(gulp.dest('./dist'));
+}
+
+gulp.task('default', ['js'])
