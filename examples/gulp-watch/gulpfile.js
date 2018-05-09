b949b4c21e349ce08ad6495cbc0a43858ed25ec2
yvan <57882634@qq.com>
2018-05-09 19:30:58 +0800

    browser-sync

--- examples/gulp-watch/gulpfile.js
+++ examples/gulp-watch/gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function() {
   return gulp.src('sass/*.scss')
+  .pipe(sourcemaps.init())
   .pipe(watch('sass/*.scss'))
-  .pipe(sass())
+  .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+  .pipe(sourcemaps.write('./maps'))
   .pipe(gulp.dest('dist'));
 });
