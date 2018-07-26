c7b478a5db889597a25d428c2421c6bdc2348d1d
yvan <57882634@qq.com>
2018-07-26 23:56:50 +0800

    mocha

--- examples/mocha/gulpfile.js
+++ examples/mocha/gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var gutil = require('gulp-util');
+var tap = require('gulp-tap');
 
 gulp.task('default', function() {
-  return gulp.src(['test/test-*.js'], {read: false})
+  return gulp.src(['test/test.spec.js']) 
+  // 此处glob没有匹配到任何文件，但是mocha依然能够正常执行，
+  // 是因为没有给mocha传递globs时，默认会执行test目录下的测试用例
+  .pipe(tap(function(file) {
+    gutil.log(file.contents.toString());
+  }))
   .pipe(mocha({
     reporter: 'spec',
     globals: {
