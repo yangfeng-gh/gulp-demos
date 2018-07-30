f0d47c28d570ec7a8df247f0208d0f51a697324e
yvan <57882634@qq.com>
2018-07-31 00:19:48 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
+// gulp --cwd example/mocha
+
 var gulp = require('gulp');
 var mocha = require('mocha');
 var config = require('./config.json')
 
 gulp.task('default', function() {
-    return gulp.src([config.mocha + './test/*.js'])
+    return gulp.src([config.mocha + 'test/*.js'])
     .pipe(mocha({
         reporter: 'list'
     }))
     .on('error', gutil.log);
 }); 
-
-// gulp --cwd example/mocha
