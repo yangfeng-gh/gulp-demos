bfa2d0181ea1517a7623b51d5caba2594088ba20
yvan <57882634@qq.com>
2018-08-03 04:24:02 +0800

    [Prerelease] Bumped version number

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,71 @@
-// gulp --cwd example/mocha
-
 var gulp = require('gulp');
-var mocha = require('mocha');
-var config = require('./config.json')
+var runSequence = require('run-sequence');
+var bump = require('gulp-bump');
+var gutil = require('gulp-util');
+var git = require('gulp-git');
+var fs = require('fs');
 
-gulp.task('default', function() {
-    return gulp.src([config.mocha + 'test/*.js'])
-    .pipe(mocha({
-        reporter: 'list'
+gulp.task('bump-version', function () {
+  // 注意：这里我硬编码了更新类型为 'patch'，但是更好的做法是用
+  //      minimist (https://www.npmjs.com/package/minimist) 通过检测一个命令行参数来判断你正在做的更新是
+  //      一个 'major'， 'minor' 还是一个 'patch'。
+  return gulp.src(['./bower.json', './package.json'])
+    .pipe(bump({
+      type: "patch"
+    }).on('error', gutil.log))
+    .pipe(gulp.dest('./'));
+});
+
+// Run git add with options
+gulp.task('git-add', function () {
+  return gulp.src('.')
+    .pipe(git.add());
+});
+
+gulp.task('git-commit', function () {
+  return gulp.src('.')
+    .pipe(git.commit('[Prerelease] Bumped version number', {
+      emitData: true
     }))
-    .on('error', gutil.log);
-}); 
+    .on('data', function (data) {
+      console.log(data);
+    });
+});
+
+gulp.task('git-push', function (cb) {
+  git.push('origin', 'master', cb);
+});
+
+gulp.task('create-new-tag', function (cb) {
+  var version = getPackageJsonVersion();
+  git.tag(version, 'Created Tag for version: ' + version, function (error) {
+    if (error) {
+      return cb(error);
+    }
+    git.push('origin', 'master', {
+      args: '--tags'
+    }, cb);
+  });
+
+  function getPackageJsonVersion() {
+    // 这里我们直接解析 json 文件而不是使用 require，这是因为 require 会缓存多次调用，这会导致版本号不会被更新掉
+    return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+  };
+});
+
+gulp.task('release', function (callback) {
+  runSequence(
+    'bump-version',
+    'git-add',
+    'git-commit',
+    'git-push',
+    'create-new-tag',
+    function (error) {
+      if (error) {
+        console.log(error.message);
+      } else {
+        console.log('RELEASE FINISHED SUCCESSFULLY');
+      }
+      callback(error);
+    });
+});
